c5a7fef2b30760836f85cc38cbaed08b6b0fb03e
Wiktor <119621878+wiktork77@users.noreply.github.com>
2024-01-26 18:18:35 +0100

    Update broker_lab.py

--- warden_lab/broker_lab.py
+++ warden_lab/broker_lab.py
@@ -1,8 +1,8 @@
 import paho.mqtt.client as mqtt
-import keyboard
 import threading
-from encryption_utils import decode_pub_key, encode_message
-from utilities import is_room_number_valid
+from encryption_utils_lab import decode_pub_key, encode_message
+from utilities_lab import is_room_number_valid
+from mfrc522 import MFRC522
 
 from config import *
 
@@ -10,13 +10,18 @@ from buzzer import buzzer
 
 from menu import init_menu, menu, display_brightness
 
-broker_address = "10.108.33.122"
+broker_address = "10.108.33.121"
 
 client = mqtt.Client()
 
-authorized = True
+authorized = False
+can_read = True
+answer = ""
 
 current_room = 0
+
+# pokoj jest liczba dodatnia, tak wiec -1 oznacza stan domyslny
+last_room_pointer = -1
 
 encoder_topic = "messages/encoder"
 keys_requests_topic = "messages/key/requests"
@@ -28,7 +33,7 @@ server_result_topic = "server/result"
 
 current_pub_key = ""
 
-allOptions = [1, 4, 7, 9]
+allOptions = []
 
 encoderLeftPrevoiusState = GPIO.input(encoderLeft)
 encoderRightPrevoiusState = GPIO.input(encoderRight)
@@ -43,43 +48,72 @@ def send_command_to_server(command):
 
 
 def unauthorized_ui():
-    while not authorized:
-    # dopoki unauthorized -> czytaj karte
-    # on authorization -> send_command_to_server(f"rooms:{uid}")
-    # wait for response, convert to list.
-    # if list is empty -> buzzer! else authorized_ui(list)
-    pass
+    global authorized
+    global can_read
+    MIFAREReader = MFRC522()
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    if status == MIFAREReader.MI_OK and can_read:
+        (status, uid) = MIFAREReader.MFRC522_Anticoll()
+        if status == MIFAREReader.MI_OK:
+            num = 0
+            for i in range(0, len(uid)):
+                num += uid[i] << (i*8)
+            can_read = False
+            send_command_to_server(f"rooms:{num}")
+            print(num)
+            authorized = True
+            timeout_thread = threading.Timer(0.4, check_for_rooms_answer)
+            timeout_thread.start()
+    else:
+        pass
 
 
-def quick_buzzer():
-    #threadami
-    pass
+def check_for_rooms_answer():
+    global can_read
+    global authorized
+    global answer
+    if answer != "":
+        can_read = False
+        authorized = True
+        print(answer)
+        # parse to rooms
+        # deleguje zarzadzanie pokojami do petli while
+        # display_available_rooms()
+    else:
+        buzzer(1)
+        timeout_thread = threading.Timer(0.2, lambda: disable_buzzer())
+        timeout_thread.start()
+        authorized = False
 
-def authorized_ui(rooms):
-    pass
+
+def disable_buzzer():
+    global can_read
+    can_read = True
+    buzzer(0)
 
 
 def management_ui():
     print(f"Managing room {current_room}")
-    pass
-
-
-def ask_for_rooms():
-    pass
 
 
 def handle_brightness_data(brightness):
     display_brightness(disp, brightness)
-    pass
 
 
 def discard_room(e):
     global current_pub_key
     global current_room
-    if current_pub_key != "":
-        print(f"Exiting room {current_room}")
-        current_room = 0
-        display_available_rooms()
+    # global last_room_pointer ?
+    # znak zapytania, bo byc moze ten pointer zachowa sie dobrze, zobaczymy.
+    if authorized:
+        if current_pub_key != "":
+            print(f"Exiting room {current_room}")
+            current_room = 0
+            # nie przechodze do zadnego ui - deleguje zarzadzanie do petli while
+            current_pub_key = ""
+            # last_room_pointer = allOptions[0]  ?
+        else:
+            logout()
 
 
 def setup_current_public_key(msg):
@@ -91,13 +125,18 @@ def setup_current_public_key(msg):
 
 def on_message_received(c, userdata, msg):
     topic = msg.topic
+    global answer
+    global allOptions
     if topic == keys_answers_topic and msg.payload.decode() != "":
         setup_current_public_key(msg)
     elif topic == brightness_status_topic:
         brightness = msg.payload.decode()
+        print(brightness)
         handle_brightness_data(brightness)
     elif topic == server_result_topic:
         print(f"Response from server: {msg.payload.decode()}")
+        answer = msg.payload.decode()
+        allOptions = parse_rooms_answer(msg.payload.decode())
 
 
 def rotation_decode(e):
@@ -117,12 +156,13 @@ def rotation_decode(e):
         # prawo
         message = str(0)
         if current_pub_key != "":
+            print("prawo")
             encoded_message = encode_message(message, current_pub_key)
             client.publish(topic=encoder_topic, payload=encoded_message)
-        else:
+        if authorized:
             first = allOptions.pop(0)
             allOptions.append(first)
-            menu(disp, allOptions)
+            # menu(disp, allOptions)
 
     if (
         encoderRightPrevoiusState == 1
@@ -133,32 +173,36 @@ def rotation_decode(e):
         # lewo
         message = str(1)
         if current_pub_key != "":
+            print("lewo")
             encoded_message = encode_message(message, current_pub_key)
             client.publish(topic=encoder_topic, payload=encoded_message)
-        else:
+        if authorized:
             last = allOptions.pop()
             allOptions = [last] + allOptions
-            menu(disp, allOptions)
+            # menu(disp, allOptions)
 
     encoderLeftPrevoiusState = encoderLeftCurrentState
     encoderRightPrevoiusState = encoderRightCurrentState
 
 
-
 def check_for_key_answer():
     if current_pub_key != "":
         management_ui()
+        # tutaj jest wyswietlanie ui przez event przeslania wiadomosci od pokoju
     else:
         print("Room unavailable")
-        # tutaj buzzer
-        # choose_room()
-
+        buzzer(1)
+        timeout_thread = threading.Timer(0.4, disable_buzzer)
+        timeout_thread.start()
 
 
 def display_available_rooms():
     global current_pub_key
-    current_pub_key = ""
-    menu(disp, allOptions)
+    global last_room_pointer
+    if len(allOptions) > 0 and last_room_pointer != allOptions[0]:
+        menu(disp, allOptions)
+        # blokada przed wielokrotnym rysowaniem na o-ledzie
+        last_room_pointer = allOptions[0]
 
 
 def choose_room(e):
@@ -190,23 +234,62 @@ def subscribe():
 
 
 def setup_broker():
-    global disp
-    disp = init_menu()
-    client.connect(broker_address, keepalive=0)
+    client.connect(broker_address, keepalive=0, port=1883)
 
     subscribe()
 
     client.on_message = on_message_received
 
     bind_controllers()
+    client.loop_start()
 
-    display_available_rooms()
 
-    client.loop_forever()
+
+def parse_rooms_answer(answer):
+    split = answer.split(',')
+    temp = []
+    for item in split:
+        if item != '':
+            temp.append(int(item))
+    print(temp)
+    return temp
+
+
+def setup_disp():
+    global disp
+    disp = init_menu()
+
+
+def logout():
+    global answer
+    global can_read
+    global authorized
+    global allOptions
+    global last_room_pointer
+    answer = ""
+    can_read = True
+    authorized = False
+    # nie ma co trzymac dostepnych pokoi poprzedniej osoby
+    allOptions = []
+    last_room_pointer = -1
+    setup_disp()
 
 
 if __name__ == "__main__":
-    unauthorized_ui()
+    setup_disp()
     setup_broker()
+    while True:
+        if not authorized: # Jesli uzytkownik nie przylozyl karty lub nie ma dostepu
+            unauthorized_ui()
+        elif authorized and current_pub_key == "": # Jesli uzytkownik ma dostep do zarzadzania, ale nie wybral pokoju
+            display_available_rooms()
+        elif authorized and current_pub_key != "": # uzytkownik wybral pokok
+            pass
+            # Tutaj to jest chyba zarzadzane eventami.
+            # Na kazda zmiane jasnosci pojawia sie klatka.
+            # Patrz linia 124 - 126
+            # Na każdą wiadomość od pokoju (jest to reakcja na rozkaz enkodera)
+            # Jest pokazywana klatka na ekranie, nie powinno być tu problemu z rozmazaniem
+
 
 
